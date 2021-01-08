fc7c75807bb82d6b3f97e42b37c3c6883a2f9d3b
Nicolas Nuzillard <nicolas.nuzillard@hubstairs.com>
2021-01-08 13:55:06 +0100

    1st commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,31 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <button onClick={()=>{
+          let ws = new WebSocket('ws://127.0.0.1:8080', 'echo-protocol');
+          ws.onopen = function (event) {
+            ws.send("Voici un texte que le serveur attend de recevoir dès que possible !");
+          };
+          ws.onerror = (e)=>{
+            console.error(e)
+          }
+          ws.onmessage = (m) => {
+            console.log('received')
+            console.log(m)
+          }
+        }
+        } >Coucou la websocket </button>
     </div>
   );
 }
+
+// <input type="range" min="0" max="10" step=".5" onChange="f_onChange"/>
+
+
+// f_onChange =function(e) {
+//   console.log('hello', e.target.value)
+// }
+
 
 export default App;
